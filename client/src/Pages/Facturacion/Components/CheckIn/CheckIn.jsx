8c84d6a481b42e4cdbc45f7edf17c49684ab2a43
J. Diego Fernández <JDFBdeveloper@gmail.com>
2022-01-22 16:03:57 -0300

    Detalles

--- client/src/Pages/Facturacion/Components/CheckIn/CheckIn.jsx
+++ client/src/Pages/Facturacion/Components/CheckIn/CheckIn.jsx
@@ -15,45 +15,42 @@ const CheckIn = ({ close, checkIn, handlePaidBill }) => {
   e.preventDefault();
     handlePaidBill()
     close()
-}
+  }
 
   return (
       <div className={styles.container} >
-        <div>
-          <h1 className={styles.title}>CHECK IN</h1>
+        <div className={styles.titleContainer}>
+          <h1 className={styles.title}>Check In</h1>
         </div>
         <div>
           <form>
-            <div className={styles.inputs}>
-            <div className={styles.inputDiv1}>
-              <label className={styles.Label} >Total Bill:{checkIn}</label>
-              <input
-                className={styles.inputMount}
-                type='text'
-                name='mount'
-                placeholder='Enter an amount'
-                value={input}
-                onChange={handleChange}                
-              />
-
+            <div className={styles.form}>
+              <div className={styles.inputDiv}>
+                <label className={styles.label} >Total: ${checkIn}</label>
+                <input
+                  className={styles.input}
+                  type='number'
+                  name='mount'
+                  placeholder='Enter an amount'
+                  value={input}
+                  onChange={handleChange}                
+                />
               </div>
               <div className={styles.inputDiv}>
-              <label
-                className={styles.Change}
-                onChange={handleChange}
-              >Change:{input && Number(input) - Number(checkIn)}</label>
-               
+                <label
+                  className={styles.label}
+                  onChange={handleChange}
+                >Change: ${input && Number(input) - Number(checkIn)}</label>
               </div>
               
-            <div className={styles.inputDiv}>
+              <div className={styles.inputDiv}>
                 {
                   <button
-                    className={styles.btnRegister}
+                    className={styles.input}
                     type="submit"
                    onClick={handleSubmit}
                   >CONFIRM</button>
                 }
-
               </div>
             </div>
           </form>
