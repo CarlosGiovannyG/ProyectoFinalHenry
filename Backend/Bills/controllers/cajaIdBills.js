7052ce5115f92658ec6570b8cca9e04af2335ad4
MrGeodude <axelcastillo690@gmail.com>
2022-01-23 15:27:58 -0600

    Fixed caja id bills logic

--- Backend/Bills/controllers/cajaIdBills.js
+++ Backend/Bills/controllers/cajaIdBills.js
@@ -4,16 +4,15 @@ const cajaIdBills = async (req, res) => {
   try {
     const { id } = req.params;
     const facturasAbiertas = await Bills.findById(id);
-    const objFactura = {};
-    objFactura.sumatotal = 0;
+    const objFactura = {sumatotal: 0, array: []};
     facturasAbiertas.products.forEach((e) => {
-      if (objFactura[e.name]) {
-        objFactura[e.name].cantidad += 1;
-        objFactura[e.name].total += Number(e.price);
-        objFactura.sumatotal += Number(e.price);
-      } else if (!objFactura[e.name]) {
-        objFactura[e.name] = { cantidad: 1, total: Number(e.price) };
-        objFactura.sumatotal += Number(e.price);
+      objFactura.sumatotal += e.price
+      const product = objFactura.array.find(p => p.name === e.name ) 
+      if(!product)
+      objFactura.array.push({name: e.name, cantidad:  1, total: e.price})
+      else{
+        product.cantidad += 1
+        product.total += e.price
       }
     });
 
